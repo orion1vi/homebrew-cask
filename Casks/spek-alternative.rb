53c1cad398f6fce4ed514c595ed596a0aed96287
Vilius <orion1vi@protonmail.com>
2020-01-23 18:53:14 +0200

    Fixed version download in spek-alternative

--- Casks/spek-alternative.rb
+++ Casks/spek-alternative.rb
@@ -2,7 +2,7 @@ cask 'spek-alternative' do
   version '0.8.2.3'
   sha256 '6426c4c34ac8bcb2fa1020cb295e3c105ca912c853cbbe84d1d196a32bba361b'
 
-  url "https://github.com/withmorten/spek-alternative/releases/download/v#{version}/spek-alternative-#{version}.dmg"
+  url "https://github.com/withmorten/spek-alternative/releases/download/#{version}/spek-alternative-#{version}.dmg"
   name 'Spek'
   homepage 'https://github.com/withmorten/spek-alternative'
 
