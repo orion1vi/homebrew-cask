d06d64ab4a3e19be7588eb8a92c4e291512f14be
Vilius <orion1vi@protonmail.com>
2020-01-23 18:50:29 +0200

    Added cask for spek-alternative

---
+++ Casks/spek-alternative.rb
@@ -0,0 +1,10 @@
+cask 'spek' do
+  version '0.8.2.3'
+  sha256 '6426c4c34ac8bcb2fa1020cb295e3c105ca912c853cbbe84d1d196a32bba361b'
+
+  url "https://github.com/withmorten/spek-alternative/releases/download/v#{version}/spek-alternative-#{version}.dmg"
+  name 'Spek'
+  homepage 'https://github.com/withmorten/spek-alternative'
+
+  app 'Spek.app'
+end
